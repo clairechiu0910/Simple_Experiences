0a007b4e411a0eb78e1944508a215041fa9d70a3
Claire Chiu Li-Ling <claire.chiu@titansoft.com.sg>
2020-05-02 17:27:56 +0800

    fill out datas

--- script.js
+++ script.js
@@ -7,25 +7,11 @@ var myData = {
         '0984200235',
         'clairechiu1997@gmail.com'
     ],
-    skill:[{
-            topic: 'C / C++',
-            description: '實作搜尋引擎、各類演算法與資料結構'
-        }, {
-            topic: 'Asp.Net / Asp.Net Core',
-            description: ''
-        }, {
-            topic: 'Vue',
-            description: ''
-        }, {
-            topic: 'MySQL',
-            description: '建立資料庫、依照需求設計有效率的搜尋指令'
-        }
-    ],
     projects:[{
             name: '利用DP實作支援模糊搜尋的搜尋網站', 
             description: [{
                     topic: '模糊搜尋', 
-                    description: '利用DP計算best distance error,並以變數控制比較字數,縮短搜尋時間。'
+                    description: '利用DP計算best distance error, 並以變數控制比較字數, 縮短搜尋時間。'
                 }, {
                     topic: '依據字串權重與關聯性顯示結果',
                     description: ''
@@ -35,35 +21,54 @@ var myData = {
             name: '以機器人與超聲波感測器建構環境地圖', 
             description: [{
                     topic: '超聲波測量牆壁夾角 (誤差小於3%)', 
-                    description: '在車上裝設兩顆超聲波,並用伺服馬達控制超聲波旋轉,量測出自走車和牆壁兩邊的最短位置,計算出牆壁夾角。'
+                    description: '在車上裝設兩顆超聲波, 並用伺服馬達控制超聲波旋轉, 量測出自走車和牆壁兩邊的最短位置, 計算出牆壁夾角。'
                 }, {
                     topic: '車子定位 (誤差小於9%)',
-                    description: '利用輪子上的編碼器,求得車子的移動速度與角速度,並對時間作積分,求得車子的座標值和方向。'
+                    description: '利用輪子上的編碼器, 求得車子的移動速度與角速度, 並對時間作積分, 求得車子的座標值和方向。'
                 }, {
                     topic: '改善車子偏移狀況',
-                    description: '利用車子當前座標與方向,及時改變車子左右輪的速度讓車子能夠在預期路徑上行走'
+                    description: '利用車子當前座標與方向, 及時改變車子左右輪的速度讓車子能夠在預期路徑上行走'
                 }, {
                     topic: '周遭環境的資訊建構',
-                    description: '利用超聲波的環境資訊,以及車子座標,計算出障礙物座標值。'
+                    description: '利用超聲波的環境資訊, 以及車子座標, 計算出障礙物座標值。'
+                }
+            ]
+        }, {
+            name: '暢銷書關鍵字對股市、債券及基金的交易量與報酬關聯',
+            description: [{
+                    topic: '資料蒐集',
+                    description: '利用Python爬金石堂網站原始碼, 再擷取財經分類每週暢銷書前100名的書名與摘要'
+                }, {
+                    topic: '資料處理',
+                    description: '建立財經相關專有名詞的自訂義辭典, 並利用Jieba來斷詞'
+                }, {
+                    topic: '關聯性分析',
+                    description: '分別取每周關鍵字次數最多的15個關鍵字與目標變數的漲跌關係'
+                }, {
+                    topic: '回歸分析',
+                    description: '分析關鍵字出現的次數, 與目標變數的關係, 並分析往前拉一週、往後拉一週、與當週的時間資料結果'
                 }
             ]
         }
     ],
-    experience:[{
+    experiences:[{
             field: 'Basic Software',
-            works: [{
-                name: '以C語言實作Linux shell',
-                description: ''
+            skills: [{
+                name: 'C / C++',
+                description: '實作Linux shell、搜尋引擎、各類演算法與資料結構' 
             }, {
-                name: '以ANTLR實作Compiler,產生MIPS Code',
-                description: ''
-            }, {
-                name: '利用MySQL建立資料庫,並設計搜尋指令',
-                description: ''
+                name: 'Compiler',
+                description: '以ANTLR實作Compiler,產生MIPS Code'
             }]
         }, {
             field: 'Machine Learning',
-            works: [{
+            skills: [{
+                name: 'Feature Matching',
+                description: '實作RootSIFT、FAST detector、ORB descriptor等，並比較優劣'
+            }, {
+                name: 'Kaggle: 房價預測',
+                description: '分析房子各參數對房價的相關係數, 結果達到前1/3'
+            }, {
                 name: '利用RNN實作數字辨識',
                 description: ''
             }, {
@@ -71,13 +76,28 @@ var myData = {
                 description: ''
             }]
         }, {
-            field: '',
-            works: [{
-                name: '',
+            field: 'Web Development',
+            skills: [{
+                name: '利用MySQL建立資料庫,並設計搜尋指令',
                 description: ''
             }, {
-                name: '',
+                name: 'Asp.Net / Asp.Net Core',
+                description: '用C#寫網頁後端；JQuery、html寫前端，用MVC架構開發'
+            }, {
+                name: 'Vue',
                 description: ''
+            }]
+        }, {
+            field: 'Firmware Development',
+            skills: [{
+                name: 'NI Single-Board RIO',
+                description: '在NI自走車上開發'
+            }, {
+                name: 'ZedBoard',
+                description: '以Verilog寫, 讓板子可以實作四則運算'
+            }, {
+                name: 'Nexys-4 DDR FPGA',
+                description: '以Verilog寫, 讓板子可以執行MIPS指令'
             }]
         }
     ],
